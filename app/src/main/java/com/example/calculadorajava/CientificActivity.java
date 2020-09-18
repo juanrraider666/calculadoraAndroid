177dab8bfeb4f45932571b72624c6a7e921115b0
Miguel Celis <mcelis@optimeconsulting.com>
2020-09-18 00:43:16 -0500

    agregamos mas calculadoras y ajustes de diseño

---
+++ app/src/main/java/com/example/calculadorajava/CientificActivity.java
@@ -0,0 +1,17 @@
+package com.example.calculadorajava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CientificActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cientific);
+    }
+    public void onClick(View v) {
+    }
+}
